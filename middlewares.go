05d954afd2cb796be1b0a71c871f13fad413618b
Lukas Bachschwell <lukas@lbsfilm.at>
2025-08-06 12:28:23 +0200

    urlpath and makefile improvements
    Signed-off-by: Lukas Bachschwell <lukas@lbsfilm.at>

---
+++ middlewares.go
@@ -0,0 +1,23 @@
+package chimp
+
+import (
+	"context"
+	"net/http"
+	"strings"
+)
+
+// Uses the refer header of th erequest to determin the current page in the templates
+func UrlPathMiddleware(baseUrl string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			parts := strings.SplitAfterN(r.Referer(), "/", 2)
+			if len(parts) > 1 {
+				ctx := context.WithValue(r.Context(), "urlpath", strings.TrimPrefix(r.URL.Path, parts[1]))
+				next.ServeHTTP(w, r.WithContext(ctx))
+			} else {
+				//ctx := context.WithValue(r.Context(), "urlpath", strings.TrimPrefix(r.URL.Path, baseURL))
+				next.ServeHTTP(w, r)
+			}
+		})
+	}
+}
