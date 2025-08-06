e9560189e511e5c2816806d43b525e7fb3c8998b
Lukas Bachschwell <lukas@lbsfilm.at>
2025-08-06 13:00:45 +0200

    urlpath handle baseurl FIX
    Signed-off-by: Lukas Bachschwell <lukas@lbsfilm.at>

--- middlewares.go
+++ middlewares.go
@@ -12,7 +12,7 @@ func UrlPathMiddleware(baseUrl string) func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			parts := strings.SplitAfterN(r.Referer(), "/", 2)
 			if len(parts) > 1 {
-				ctx := context.WithValue(r.Context(), "urlpath", strings.TrimPrefix(strings.TrimPrefix(r.URL.Path, parts[1]), baseUrl))
+				ctx := context.WithValue(r.Context(), "urlpath", strings.TrimPrefix(parts[1], baseUrl))
 				next.ServeHTTP(w, r.WithContext(ctx))
 			} else {
 				//ctx := context.WithValue(r.Context(), "urlpath", strings.TrimPrefix(r.URL.Path, baseURL))
