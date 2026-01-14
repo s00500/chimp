212c9f98c38bc166fa7531d3f90392c0edcdde7c
Lukas Bachschwell <lukas@lbsfilm.at>
2026-01-14 12:28:59 +0100

    Use context in Base hansler
    Signed-off-by: Lukas Bachschwell <lukas@lbsfilm.at>

--- base.go
+++
@@ -1,8 +0,0 @@
-package chimp
-
-import "github.com/a-h/templ"
-
-// Base inserts a base tag, removing the need from many other places to use BaseURL
-func Base(baseUrl string) templ.Component {
-	return templ.Raw(`<base href=` + templ.URL(baseUrl) + `></div>`)
-}
