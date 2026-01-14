99e63bb02b1429845d7f2dc27e4639a7d6779d26
Lukas Bachschwell <lukas@lbsfilm.at>
2026-01-14 18:46:36 +0100

    Slots without root / middleware
    Signed-off-by: Lukas Bachschwell <lukas@lbsfilm.at>

--- components/slots.go
+++ components/slots.go
@@ -1,153 +1,180 @@
 package components
 
-// type ctxKey string
+import (
+	"context"
+	"io"
+	"strings"
+	"sync/atomic"
 
-// const (
-// 	ctxKeySlot     ctxKey = "slot"
-// 	ctxKeyTemplate ctxKey = "template"
-// )
+	"github.com/a-h/templ"
+)
 
-// func Root() templ.Component {
-// 	return templ.ComponentFunc(func(ctx context.Context, w io.Writer) error {
-// 		ctx = addSlotData(ctx)
-// 		return templ.GetChildren(ctx).Render(ctx, w)
-// 	})
-// }
+type ctxKey string
 
-// var slotIndex int64
+const (
+	ctxKeySlot     ctxKey = "slot"
+	ctxKeyTemplate ctxKey = "template"
+)
 
-// type None struct{}
+var slotIndex int64
 
-// func addSlotData(ctx context.Context) context.Context {
-// 	sd := make(map[int64][]interface{})
-// 	td := make(map[int64][]interface{})
-// 	ctx = context.WithValue(ctx, ctxKeySlot, &sd)
-// 	ctx = context.WithValue(ctx, ctxKeyTemplate, &td)
-// 	return ctx
-// }
+type None struct{}
 
-// func mustGetSlotData(ctx context.Context) *map[int64][]interface{} {
-// 	sd, ok := ctx.Value(ctxKeySlot).(*map[int64][]interface{})
-// 	if !ok {
-// 		panic("slot data not found")
-// 	}
-// 	return sd
-// }
+func addSlotData(ctx context.Context) context.Context {
+	sd := make(map[int64][]interface{})
+	td := make(map[int64][]interface{})
+	ctx = context.WithValue(ctx, ctxKeySlot, &sd)
+	ctx = context.WithValue(ctx, ctxKeyTemplate, &td)
+	return ctx
+}
 
-// func mustGetTemplateData(ctx context.Context) *map[int64][]interface{} {
-// 	td, ok := ctx.Value(ctxKeyTemplate).(*map[int64][]interface{})
-// 	if !ok {
+func ensureSlotData(ctx context.Context) context.Context {
+	if _, ok := ctx.Value(ctxKeySlot).(*map[int64][]interface{}); ok {
+		return ctx
+	}
+	return addSlotData(ctx)
+}
 
-// 		panic("template data not found")
-// 	}
-// 	return td
-// }
+func getSlotData(ctx context.Context) *map[int64][]interface{} {
+	sd, ok := ctx.Value(ctxKeySlot).(*map[int64][]interface{})
+	if !ok {
+		return nil
+	}
+	return sd
+}
 
-// type Slot[T any] struct {
-// 	id int64
-// }
+func getTemplateData(ctx context.Context) *map[int64][]interface{} {
+	td, ok := ctx.Value(ctxKeyTemplate).(*map[int64][]interface{})
+	if !ok {
+		return nil
+	}
+	return td
+}
 
-// func DefineSlot[T any]() *Slot[T] {
-// 	return &Slot[T]{
-// 		id: atomic.AddInt64(&slotIndex, 1),
-// 	}
-// }
+type Slot[T any] struct {
+	id int64
+}
 
-// func (s *Slot[T]) addValue(ctx context.Context, v ...T) {
-// 	sd := mustGetSlotData(ctx)
-// 	if _, ok := (*sd)[s.id]; !ok {
-// 		(*sd)[s.id] = make([]interface{}, 0)
-// 	}
-// 	if len(v) > 0 {
-// 		(*sd)[s.id] = append((*sd)[s.id], v[0])
-// 	} else {
-// 		(*sd)[s.id] = append((*sd)[s.id], nil)
-// 	}
-// }
+func DefineSlot[T any]() *Slot[T] {
+	return &Slot[T]{
+		id: atomic.AddInt64(&slotIndex, 1),
+	}
+}
 
-// func (s *Slot[T]) hasValue(ctx context.Context) bool {
-// 	sd := mustGetSlotData(ctx)
-// 	return len((*sd)[s.id]) > 0
-// }
+func (s *Slot[T]) addValue(ctx context.Context, v ...T) {
+	sd := getSlotData(ctx)
+	if sd == nil {
+		return
+	}
+	if _, ok := (*sd)[s.id]; !ok {
+		(*sd)[s.id] = make([]interface{}, 0)
+	}
+	if len(v) > 0 {
+		(*sd)[s.id] = append((*sd)[s.id], v[0])
+	} else {
+		(*sd)[s.id] = append((*sd)[s.id], nil)
+	}
+}
 
-// func (s *Slot[T]) removeValue(ctx context.Context) {
-// 	sd := mustGetSlotData(ctx)
-// 	if !s.hasValue(ctx) {
-// 		panic("slot not found")
-// 	}
-// 	(*sd)[s.id] = (*sd)[s.id][:len((*sd)[s.id])-1]
-// }
+func (s *Slot[T]) hasValue(ctx context.Context) bool {
+	sd := getSlotData(ctx)
+	if sd == nil {
+		return false
+	}
+	return len((*sd)[s.id]) > 0
+}
 
-// func (ts *Slot[T]) addTemplate(ctx context.Context, t templ.Component) {
-// 	td := mustGetTemplateData(ctx)
-// 	if _, ok := (*td)[ts.id]; !ok {
-// 		(*td)[ts.id] = make([]interface{}, 0)
-// 	}
-// 	(*td)[ts.id] = append((*td)[ts.id], t)
-// }
+func (s *Slot[T]) removeValue(ctx context.Context) {
+	sd := getSlotData(ctx)
+	if sd == nil || !s.hasValue(ctx) {
+		return
+	}
+	(*sd)[s.id] = (*sd)[s.id][:len((*sd)[s.id])-1]
+}
 
-// func (ts *Slot[T]) hasTemplate(ctx context.Context) bool {
-// 	td := mustGetTemplateData(ctx)
-// 	return len((*td)[ts.id]) > 0
-// }
+func (ts *Slot[T]) addTemplate(ctx context.Context, t templ.Component) {
+	td := getTemplateData(ctx)
+	if td == nil {
+		return
+	}
+	if _, ok := (*td)[ts.id]; !ok {
+		(*td)[ts.id] = make([]interface{}, 0)
+	}
+	(*td)[ts.id] = append((*td)[ts.id], t)
+}
 
-// func (ts *Slot[T]) removeTemplate(ctx context.Context) {
-// 	td := mustGetTemplateData(ctx)
-// 	if !ts.hasTemplate(ctx) {
-// 		panic("template not found")
-// 	}
-// 	(*td)[ts.id] = (*td)[ts.id][:len((*td)[ts.id])-1]
-// }
+func (ts *Slot[T]) hasTemplate(ctx context.Context) bool {
+	td := getTemplateData(ctx)
+	if td == nil {
+		return false
+	}
+	return len((*td)[ts.id]) > 0
+}
 
-// func (ts *Slot[T]) getTemplate(ctx context.Context) templ.Component {
-// 	td := mustGetTemplateData(ctx)
-// 	return (*td)[ts.id][len((*td)[ts.id])-1].(templ.Component)
-// }
+func (ts *Slot[T]) removeTemplate(ctx context.Context) {
+	td := getTemplateData(ctx)
+	if td == nil || !ts.hasTemplate(ctx) {
+		return
+	}
+	(*td)[ts.id] = (*td)[ts.id][:len((*td)[ts.id])-1]
+}
 
-// func (s *Slot[T]) Value(ctx context.Context) T {
-// 	sd := mustGetSlotData(ctx)
-// 	if !s.hasValue(ctx) {
-// 		panic("slot not found")
-// 	}
-// 	return (*sd)[s.id][len((*sd)[s.id])-1].(T)
-// }
+func (ts *Slot[T]) getTemplate(ctx context.Context) templ.Component {
+	td := getTemplateData(ctx)
+	if td == nil {
+		return nil
+	}
+	return (*td)[ts.id][len((*td)[ts.id])-1].(templ.Component)
+}
 
-// func (s *Slot[T]) WithValue(v T) templ.Component {
-// 	if _, ok := any(v).(None); ok {
-// 		return s
-// 	}
-// 	return templ.ComponentFunc(func(ctx context.Context, w io.Writer) error {
-// 		s.addValue(ctx, v)
-// 		c := new(strings.Builder)
-// 		templ.GetChildren(ctx).Render(ctx, c)
-// 		defer s.removeValue(ctx)
-// 		if s.hasTemplate(ctx) {
-// 			defer s.removeTemplate(ctx)
-// 			return s.getTemplate(ctx).Render(ctx, w)
-// 		}
-// 		_, err := io.Copy(w, strings.NewReader(c.String()))
-// 		return err
-// 	})
-// }
+func (s *Slot[T]) Value(ctx context.Context) T {
+	sd := getSlotData(ctx)
+	if sd == nil || !s.hasValue(ctx) {
+		var zero T
+		return zero
+	}
+	return (*sd)[s.id][len((*sd)[s.id])-1].(T)
+}
 
-// func (s *Slot[T]) Render(ctx context.Context, w io.Writer) error {
-// 	s.addValue(ctx)
-// 	c := new(strings.Builder)
-// 	templ.GetChildren(ctx).Render(ctx, c)
-// 	defer s.removeValue(ctx)
-// 	if s.hasTemplate(ctx) {
-// 		defer s.removeTemplate(ctx)
-// 		return s.getTemplate(ctx).Render(ctx, w)
-// 	}
-// 	_, err := io.Copy(w, strings.NewReader(c.String()))
-// 	return err
-// }
+func (s *Slot[T]) WithValue(v T) templ.Component {
+	if _, ok := any(v).(None); ok {
+		return s
+	}
+	return templ.ComponentFunc(func(ctx context.Context, w io.Writer) error {
+		ctx = ensureSlotData(ctx)
+		s.addValue(ctx, v)
+		c := new(strings.Builder)
+		templ.GetChildren(ctx).Render(ctx, c)
+		defer s.removeValue(ctx)
+		if s.hasTemplate(ctx) {
+			defer s.removeTemplate(ctx)
+			return s.getTemplate(ctx).Render(ctx, w)
+		}
+		_, err := io.Copy(w, strings.NewReader(c.String()))
+		return err
+	})
+}
 
-// func (s *Slot[T]) Template() templ.Component {
-// 	return templ.ComponentFunc(func(ctx context.Context, w io.Writer) error {
-// 		if s.hasValue(ctx) {
-// 			s.addTemplate(ctx, templ.GetChildren(ctx))
-// 		}
-// 		return nil
-// 	})
-// }
+func (s *Slot[T]) Render(ctx context.Context, w io.Writer) error {
+	ctx = ensureSlotData(ctx)
+	s.addValue(ctx)
+	c := new(strings.Builder)
+	templ.GetChildren(ctx).Render(ctx, c)
+	defer s.removeValue(ctx)
+	if s.hasTemplate(ctx) {
+		defer s.removeTemplate(ctx)
+		return s.getTemplate(ctx).Render(ctx, w)
+	}
+	_, err := io.Copy(w, strings.NewReader(c.String()))
+	return err
+}
+
+func (s *Slot[T]) Template() templ.Component {
+	return templ.ComponentFunc(func(ctx context.Context, w io.Writer) error {
+		ctx = ensureSlotData(ctx)
+		if s.hasValue(ctx) {
+			s.addTemplate(ctx, templ.GetChildren(ctx))
+		}
+		return nil
+	})
+}
--- components/tests_templ.go
+++ components/tests_templ.go
@@ -8,21 +8,89 @@ package components
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
-// var Dual1 = DefineSlot[string]()
-// var Dual2 = DefineSlot[string]()
+var Dual1 = DefineSlot[string]()
+var Dual2 = DefineSlot[string]()
 
-// templ DualSlot() {
-// 	<h1>
-// 		@Dual1 {
-// 			{ children... }
-// 		}
-// 	</h1>
-// 	<p>
-// 		@Dual2 {
-// 			{ children... }
-// 		}
-// 	</p>
-// }
+func DualSlot() templ.Component {
+	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
+			return templ_7745c5c3_CtxErr
+		}
+		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+		if !templ_7745c5c3_IsBuffer {
+			defer func() {
+				templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+				if templ_7745c5c3_Err == nil {
+					templ_7745c5c3_Err = templ_7745c5c3_BufErr
+				}
+			}()
+		}
+		ctx = templ.InitializeContext(ctx)
+		templ_7745c5c3_Var1 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var1 == nil {
+			templ_7745c5c3_Var1 = templ.NopComponent
+		}
+		ctx = templ.ClearChildren(ctx)
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<h1>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Var2 := templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+			templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+			templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+			if !templ_7745c5c3_IsBuffer {
+				defer func() {
+					templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+					if templ_7745c5c3_Err == nil {
+						templ_7745c5c3_Err = templ_7745c5c3_BufErr
+					}
+				}()
+			}
+			ctx = templ.InitializeContext(ctx)
+			templ_7745c5c3_Err = templ_7745c5c3_Var1.Render(ctx, templ_7745c5c3_Buffer)
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			return nil
+		})
+		templ_7745c5c3_Err = Dual1.Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "</h1><p>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Var3 := templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+			templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+			templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+			if !templ_7745c5c3_IsBuffer {
+				defer func() {
+					templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+					if templ_7745c5c3_Err == nil {
+						templ_7745c5c3_Err = templ_7745c5c3_BufErr
+					}
+				}()
+			}
+			ctx = templ.InitializeContext(ctx)
+			templ_7745c5c3_Err = templ_7745c5c3_Var1.Render(ctx, templ_7745c5c3_Buffer)
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			return nil
+		})
+		templ_7745c5c3_Err = Dual2.Render(templ.WithChildren(ctx, templ_7745c5c3_Var3), templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "</p>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		return nil
+	})
+}
 
 //	templ TStoryTest() {
 //		@TStory() {
