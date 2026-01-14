c325d07cfb3210d4030722ad14f698e95e690906
Lukas Bachschwell <lukas@lbsfilm.at>
2026-01-14 12:29:31 +0100

    JS Helpers
    Signed-off-by: Lukas Bachschwell <lukas@lbsfilm.at>

--- templ.go
+++ templ.go
@@ -16,3 +16,19 @@ func StyleWithCacheBust(url string, cacheBust bool) templ.Component {
 		return templ.Raw(`<link rel="stylesheet" href="` + templ.EscapeString(url) + `"/>`)
 	}
 }
+
+func JsWithCacheBust(url string, cacheBust bool) templ.Component {
+	if cacheBust {
+		return templ.Raw(`<script src="` + fmt.Sprintf("%s?v=%d", templ.EscapeString(url), rand.Int()) + `"></script>`)
+	} else {
+		return templ.Raw(`<script src="` + templ.EscapeString(url) + `"></script>`)
+	}
+}
+
+func JsModuleWithCacheBust(url string, cacheBust bool) templ.Component {
+	if cacheBust {
+		return templ.Raw(`<script type="module" src="` + fmt.Sprintf("%s?v=%d", templ.EscapeString(url), rand.Int()) + `"></script>`)
+	} else {
+		return templ.Raw(`<script type="module" src="` + templ.EscapeString(url) + `"></script>`)
+	}
+}
